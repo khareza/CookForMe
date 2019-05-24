d497a6feaf423ad5cc5daa73a43a069ad8289899
unknown <kml.hareza@gmail.com>
2019-05-24 13:30:28 +0200

    Added methods that send requests to api

---
+++ CookForMe/ClientApp/src/Helpers/AuthMethods.js
@@ -0,0 +1,83 @@
+﻿import decode from 'jwt-decode';
+import axios from 'axios';
+
+export default class AuthMethods {
+    constructor() {
+        this.userApiUrl = "/api/User";
+        this.authorizationApiUrl = "/api/Authorization";
+    //localhost:64763/
+    //localhost:50123/
+    }
+
+    login = (loginFormData) => {
+        return axios.post(`${this.authorizationApiUrl}/login`, loginFormData)
+            .then(res => {
+                this.setToken(res.data.token);
+                return Promise.resolve(res);
+            })
+    }
+
+    logout = () => {
+        localStorage.removeItem("id_token");
+    };
+
+
+    loggedIn = () => {
+        // Checks if there is a saved token and it's still valid
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token);
+    };
+
+    register = (registerFormData) => {
+        axios.defaults.headers.common['Authorization'] =
+            'Bearer ' + this.getToken();
+        return axios.post(`${this.authorizationApiUrl}/Register`, registerFormData);
+    }
+
+    isTokenExpired = token => {
+
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else return false;
+        } catch (err) {
+            console.log("Token validation failed");
+            return false;
+        }
+    };
+
+    setToken = idToken => {
+        localStorage.setItem("id_token", idToken);
+    };
+
+    getToken = () => {
+        return localStorage.getItem("id_token");
+    };
+
+    getConfirm = () => {
+        // Check token validation
+        let answer = decode(this.getToken());
+        console.log("Recieved answer!");
+        return answer;
+    };
+
+    editOrder = (editFormData) => {
+        axios.defaults.headers.common['Authorization'] =
+            'Bearer ' + this.getToken();
+        return axios.put(`${this.userApiUrl}/EditOrder`, editFormData);
+    }
+
+    deleteOrder = (id) => {
+        axios.defaults.headers.common['Authorization'] =
+            'Bearer ' + this.getToken();
+        console.log(id);
+        return axios.delete(`${this.userApiUrl}/DeleteOrder/` + id);
+    }
+    getUsers = () => {
+        axios.defaults.headers.common['Authorization'] =
+            'Bearer ' + this.getToken();
+        return axios.get(`${this.userApiUrl}/GetOrders`);
+    }
+
+}
