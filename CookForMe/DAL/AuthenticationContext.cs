53f4841b195fb4479aed2d27c7f66b2834041efc
unknown <kml.hareza@gmail.com>
2019-05-24 13:04:45 +0200

    Added db context and jwt token authorization

---
+++ CookForMe/DAL/AuthenticationContext.cs
@@ -0,0 +1,23 @@
+﻿using CookForMe.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookForMe.DAL
+{
+    public class AuthenticationContext : IdentityDbContext
+    {
+        public AuthenticationContext(DbContextOptions options ) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Response> Responses { get; set; }
+    }
+}
--- CookForMe/Startup.cs
+++ CookForMe/Startup.cs
@@ -1,9 +1,18 @@
+using CookForMe.AppSettings;
+using CookForMe.DAL;
+using CookForMe.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
 
 namespace CookForMe
 {
@@ -19,12 +28,56 @@ namespace CookForMe
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //get access to appsettings.json settings everywhere in applications by using ApplicationSettings class object
+            services.Configure<ApplicationSettings>(Configuration.GetSection("ApplicationSettings"));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
+            });
+
+            //Injecting db context with local connection string
+            services.AddDbContext<AuthenticationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LocalConnectionString")));
+            //??
+            services.AddDefaultIdentity<AppUser>()
+                .AddEntityFrameworkStores<AuthenticationContext>();
+
+            //Overwrite default validation
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;//change it
+
+            });
+
+            //Enable requests from anknown sources
+            services.AddCors();
+
+            var key = Encoding.UTF8.GetBytes(Configuration["ApplicationSettings:JwtKey"].ToString());
+
+            //Configuration for Jwt token authentication
+            services.AddAuthentication(x => {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(x => {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = false;
+                x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                };
             });
         }
 
@@ -39,7 +92,14 @@ namespace CookForMe
             {
                 app.UseExceptionHandler("/Error");
             }
+            app.UseCors(builders =>
+                builders.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
 
+            //Check authentication
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
