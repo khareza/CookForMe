3e7893a8baab7e0b90a2159e8fc195139c6c77f2
unknown <kml.hareza@gmail.com>
2019-05-24 13:31:22 +0200

    Added basic styles and side menu component

---
+++ CookForMe/ClientApp/src/components/SideMenu.jsx
@@ -0,0 +1,42 @@
+﻿import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import '../ComponentsStyles/SideMenu.css';
+
+export class SideMenu extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true
+        };
+    }
+
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+
+    render() {
+        return (
+            <header className="header" role="banner">
+                <h1 className="logo">
+                    <a href="/profile">Your <span>Voter</span></a>
+                </h1>
+                <div className="nav-wrap">
+                    <nav className="main-nav">
+                        <ul className="unstyled list-hover-slide">
+                            <li><NavLink to="/profile">My profile</NavLink></li>
+                            <li><NavLink to="/orders">Orders</NavLink></li>
+                            <li><NavLink to="/responses">Responses</NavLink></li>
+                        </ul>
+                    </nav>
+                    
+                </div>
+                <button className="btn btn-danger logOutBtn" onClick={this.props.logOut}>LogOut</button>
+            </header>
+        );
+    }
+}
